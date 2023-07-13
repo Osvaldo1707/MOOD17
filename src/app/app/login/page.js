01a87705b70f810f4bb64d65c36d16c1bac203d6
Manuel Osvaldo Orozco Delgado <theorderofthefoxers.167@gmail.com>
2023-07-12 22:36:45 -0600

    Commit inicial

---
+++ src/app/app/login/page.js
@@ -0,0 +1,25 @@
+
+const [state, setState] = useState({
+    email:""
+    password 
+});
+
+const handleChange = (event) => {
+    const { name, value } = event.target;
+    setState ({
+        ...state,
+        [name]: value 
+        })    
+        
+}
+
+return (
+    <div className="container"> 
+
+        <input onChange = {handleChange} name="email" value={state.email} type="text" placeholder="email"/>
+        <input type="password" placeholder="123456" />
+
+        <button>Login</button> 
+
+    </div>
+)
