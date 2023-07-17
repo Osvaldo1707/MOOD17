3fd6bcce2d9daac777c851998b9e0dcdc34f9f1a
Manuel Osvaldo Orozco Delgado <theorderofthefoxers.167@gmail.com>
2023-07-17 07:46:38 -0600

    Cambios de archivos

--- src/app/app/login/page.js
+++ src/app/app/login/page.js
@@ -1,12 +1,18 @@
 "use client"
 import { useState } from "react";
-import "./login.css"
+import{TextField, Stack} from '@mui/material';
+import { login } from "../services/firebase/auth";
 
-export default function Login() 
+
+
+
+export default function Login() {
 const [state, setState] = useState({
     email:"",
     password,
 });
+
+
 
 const handleChange = (event) => {
     const { name, value } = event.target;
@@ -17,14 +23,38 @@ const handleChange = (event) => {
         
 }
 
+const onLogin = () => {
+    const { email, password} = state;
+    login(email, password);
+}
+
+}
+
 
 return (
-    <div className="container"> 
+    <Stack
+        width="500px"
+        spacing={2} // int 1 = donde 1 es 8px
+        py="10px"
+        m={4}
+    
+    > 
 
-        <input onChange = {handleChange} name="email" value={state.email} type="text" placeholder="email"/>
-        <input type="password" placeholder="123456" />
+        <TextField onChange = {handleChange} 
+        name="email" 
+        value={state.email} 
+        type="text" 
+        placeholder="email"/>
+        <TextField type="password"
+         placeholder="123456" 
+         />
+        <TextField
+        name="password"
+        onChange={handleChange}
+        type="password"
+        label="123456"
+        />
+        <button variant="outlined" onClick={onLogin}>Login</button> 
 
-        <button>Login</button> 
-
-    </div>
+    </Stack>
 )
